a2142deda56e3c1c4607f8be4287f5e6844f12a9
VonMagombo <mvmagombo@gmail.com>
2023-07-25 07:50:13 +0200

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * swap_int - swaps the values of the two variables with the aid of a temporary variable.
+ *swap_int - swaps the values of the two variables with the aid of a temporary variable.
  *@a: integer to swap
  *@b: integer to swap
  *Return: Always 0.
