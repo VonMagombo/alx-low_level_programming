37398190b88874f5dcc15a90ef9914877fc815a0
VonMagombo <mvmagombo@gmail.com>
2023-07-25 09:16:32 +0200

    ./6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,19 +2,15 @@
 #include <string.h>
 /**
  * puts2 - prints every other character of a string, starting with the first character followed by a new line.
+ * @str: input
  * Return: void.
  */
-void puts2(char *str)
-{
-	int i, midpoint;
-	int multiply;
-
-	midpoint = (strlen(str)-1)/2;
-
-	for(i = 0; i <= midpoint; i++)
+void puts2(char *str) {
+	int i = 0;
+	while (str[i] != '\0') 
 	{
-		multiply = (str[i] * 2);
-		_putchar(multiply);
-	}
-	_putchar('\n');
+		_putchar(str[i]);
+        	i += 2;
+        }
+    	_putchar('\n');
 }
