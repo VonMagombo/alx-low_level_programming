16f8d4716da742a2a6cf8f152a837226eb6c3bf3
VonMagombo <mvmagombo@gmail.com>
2023-07-25 09:55:31 +0200

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,16 @@
 #include <string.h>
 /**
  * _strlen - checks if the parameter is a string
- * @*s
+ * @*s: input
  * Return: length of the string
  */
 int _strlen(char *s)
 {
-return strlen(s);
+	int len = 0;
+	while(*s != '\0')
+	{
+		len++;
+		s++;
+	}
+return (len);
 }
