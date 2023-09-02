f775b230207be6d1ea5de2717eb02e3264fbb4ef
VonMagombo <mvmagombo@gmail.com>
2023-09-02 04:40:38 +0200

    binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * binary_to_uint - converts a number to binary
+ * @b: parameter
+ * Return: number converted or 0 if there is no binary valuei.
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -11,17 +11,15 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
         return (0);
-
     	while (b[i] != '\0')
 	{
         	if (b[i] != '0' && b[i] != '1')
 		{
-            		return 0;  // Invalid character found
+            		return 0;
         	}
-        	result = (result << 1) | (b[i] - '0'); // Shift left and add the binary digit
+        	result = (result << 1) | (b[i] - '0'); 
         	i++;
     	}
-
 	return (result);
 }
 
